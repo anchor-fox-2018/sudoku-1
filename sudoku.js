cf79ab21a282319ed58a92fd6f83a9f4840a08f0
Lundy Orlando <simplyeazy@gmail.com>
2018-09-07 17:50:41 +0700

    Release 0

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,42 @@
-"use strict"
-
+"use strict";
+const DIM = 9;
+const BLANK = 0;
+const SPACE = ' ';
+const LINE = '|';
+const NEW_ROW = "-------------------------------------";
+/*
+"
+105802000
+090076405
+200400819
+019007306
+762083090
+000061050
+007600030
+430020501
+600308900
+"
+ */
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+  }
+  board(){
+    let board_string = `105802000090076405200400819019007306762083090000061050007600030430020501600308900`
+    let X = [];
 
-  solve() {}
-
-  // Returns a string representing the current state of the board
-  board() {}
+    for (let i = 0, lastPosition = 0; i <9; i++) {
+      let arrCol = [];
+      for (let j = 0; j < 9; j++) {
+        arrCol.push(board_string[lastPosition]);
+        lastPosition++;
+      }
+      X.push(arrCol);
+    }
+    console.log(X);
+  }
+  solve() { }
+  checkVertical() { }
+  checkHorizontal() { }
 }
 
 // The file has newlines at the end of each line,
@@ -19,6 +49,6 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+//game.solve()
 
 console.log(game.board())
