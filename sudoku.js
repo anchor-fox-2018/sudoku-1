fde78218fbdae8373ca9d8a30bffa42aa66b57b6
muhamadabduh <muhamad.abduh.muh@gmail.com>
2018-09-08 17:14:01 +0700

    sudoku first commit, please wait for my best result

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,36 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    let output = [];
+    let count = 0;
+    for(let i = 0; i < 9; i++){
+      let row = [];
+      for(let j = 0; j < 9; j++){
+        row.push(board_string[count]);
+        count++
+      }
+      output.push(row);
+    }
+    return output;
+  }
+
+  findCell(){
+    let cellToCheck = [];
+
+    // for(let i = 0; i < b)
+  }
+  checkHorizontal(col){
+    // for(let i = 0; i <  )
+  }
+
 }
 
 // The file has newlines at the end of each line,
@@ -18,6 +42,7 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 
 var game = new Sudoku(board_string)
 
+console.log(board_string);
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
 
