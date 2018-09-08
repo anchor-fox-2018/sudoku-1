5f273405adbace347ab38f5720c2ae2c70c054d6
Dody Herawan <dody@Dodys-MacBook-Pro.local>
2018-09-08 18:31:00 +0700

    Release 0

--- sudoku.js
+++ sudoku.js
@@ -22,9 +22,11 @@ class Sudoku {
           let temp = {
             currentValue: board[i][c],
             col: i,
-            row: c
+            row: c,
+            numbers: '123456789'
           }
           zeroPositions.push(temp);
+          console.log(zeroPositions)
         }
       }
 
@@ -37,54 +39,46 @@ class Sudoku {
     console.log(board) ;
   
     let threeThree = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
-    
-    //col
-    for (let i = 0; i < board.length; i++) {
 
-      //row
-      for (let c = 0; c < board[i].length; c++) {
+      //zeroPositions
+    for (let c = 0; c < zeroPositions.length; c++) {
+      let zP = zeroPositions[c].numbers;
+      console.log(zeroPositions[c][0])
 
-        
-
-        if (board[i][c] === '0') {
-        let number = '123456789';
-
-          //remove number from horizontal
-          for (let f = 0; f < board[i].length; f++) {
-            if (board[i][f] !== '0') {
-              number = number.replace(board[i][f].toString(), '');
-            }
-          }
-
-          //remove number from vertical
-          for (let f = 0; f < board[i].length; f++) {
-            if (board[f][c] !== '0') {
-              number = number.replace(board[f][c].toString(), '');
-            }
-          }
-
-          //remove number from 3x3 box
-          let targetCol = threeThree[Math.floor(i/3)];
-          let targetRow = threeThree[Math.floor(c/3)];
-
-          for (let n = 0; n < 3; n++) {
-            for (let r = 0; r < 3; r++) {
-              if (board[targetCol[n]][targetRow[r]] !== '0') {
-                number = number.replace(board[targetCol[n]][targetRow[r]].toString(), '');
-              }
-
-            }
-          }
-
-          // changing the number from 0 to something
-          if (number.length > 0) {
-            console.log('before: ' + board[i][c]);
-            board[i][c] = number[0];
-            console.log('after: ' + board[i][c]);
+        //remove number from horizontal
+        for (let f = 0; f < board[c].length; f++) {
+          if (board[c][board[zeroPositions[c].row]] !== '0') {
+            zP = zP.replace(board[zeroPositions[c].col][zeroPositions[c].row], '');
           }
         }
-      }
 
+        //remove number from vertical
+        for (let f = 0; f < board[c].length; f++) {
+          if (board[zeroPositions[c].col][board[zeroPositions[c].row]] !== '0') {
+            zP = zP.replace(board[zeroPositions[c].col][board[zeroPositions[c].row]].toString(), '');
+          }
+        }
+
+        //remove number from 3x3 box
+        let targetCol = threeThree[Math.floor(i/3)];
+        let targetRow = threeThree[Math.floor(c/3)];
+
+        for (let n = 0; n < 3; n++) {
+          for (let r = 0; r < 3; r++) {
+            if (board[zeroPositions[c].col][board[zeroPositions[c].row]] !== '0') {
+              zP = zP.replace(board[targetCol[n]][targetRow[r]].toString(), '');
+            }
+
+          }
+        }
+
+        // changing the number from 0 to something
+      if (zP.length > 0) {
+        board[zeroPositions[c].col][board[zeroPositions[c].row]] = zeroPositiosn[c].number[0];
+          c++;
+        }
+
+        
     }
 
     return board;
