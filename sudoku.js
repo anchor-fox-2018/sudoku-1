083a333df864cbfefc9d2000e9fab7ee109ceb76
Lundy Orlando <simplyeazy@gmail.com>
2018-09-08 23:05:36 +0700

    Release 0 Sudah benar kodingnya tapi OOPnya belum jadi

--- sudoku.js
+++ sudoku.js
@@ -1,88 +1,111 @@
 "use strict";
-const BLANK = '0';
+const BLANK = 0;
 const SPACE = ' ';
 const LINE = '|';
 const NEW_ROW = "-------------------------------------";
 class Sudoku {
   constructor(board_string) {
+    this._board_string = board_string;
   }
-  board() {
-    let board_string = `105802000090076405200400819019007306762083090000061050007600030430020501600308900`
-    // Fill the 
+  processStringToArray(board_string) {
+    //let board_string = `105802000090076405200400819019007306762083090000061050007600030430020501600308900`
+    // Fill the array with boardstring data
     let arrResult = [];
     for (let i = 0, lastPosition = 0; i < 9; i++) {
       let arrCol = [];
       for (let j = 0; j < 9; j++) {
-        arrCol.push(board_string[lastPosition]);
+        arrCol.push(Number(board_string[lastPosition]));
         lastPosition++;
       }
       arrResult.push(arrCol);
     }
-    console.log(arrResult);
-    // check horizontal
-    this.solve(arrResult);
+    return arrResult;
   }
-
-  solve(array) {
+  printGrid(array) {
+    for (let i = 0; i < array.length; i++) {
+      let printInLine = "";
+      for (let j = 0; j < array.length; j++) {
+        printInLine = printInLine + array[i][j];
+        //console.log(array[i][j]);
+      }
+      //console.log('n\n');
+    }
+  }
+  board() {
+    if (this.solveSudoku(this.processStringToArray())) {
+      this.printGrid(solvedSudoku)
+    }
+    else {
+      console.log('Ga bisa boss');
+    }
+  }
+  checkBlankElement(array, locationList) {
     for (let row = 0; row < array.length; row++) {
       for (let col = 0; col < array.length; col++) {
-        if (array[row][col]===BLANK) {
-          console.log(`Cell ${row} ${col} is BLANK`)
-          // masih ngebug
-        }
-      }
-    }
-
-    for (let num = 1; num <= 9; num++) {
-
-    }
-  }
-
-  checkVertical(array, col, num) {
-    for (let row = 0; row < array.length; row++) {
-      if (array[row][col] === num) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  checkHorizontal(array, row, num) {
-    for (let col = 0; col < array.length; col++) {
-      if (array[row][col] === num) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  checkBox3x3(array, boxStartRow, boxStartCol, num) {
-    for (let row = 0; row < 3; row++) {
-      for (let col = 0; col < 3; col++) {
-        if (array[row + boxStartRow][col + boxStartCol] === num) {
+        if (array[row][col] === BLANK) {
+          locationList[0] = row;
+          locationList[1] = col;
           return true;
         }
       }
     }
     return false;
   }
-
-  isSafe(array, row, col, num) {
-    return
-    !this.checkHorizontal(array, row, num) &&
-      !this.checkVertical(array, row, num) &&
+  checkRow(array, row, num) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[row][i] === num) {
+        return true;
+      }
+    }
+    return false;
+  }
+  checkColumn(array, col, num) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i][col] === num) {
+        return true;
+      }
+    }
+    return false;
+  }
+  checkBox3x3(array, boxStartRow, boxStartCol, num) {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (array[i + boxStartRow][j + boxStartCol] === num) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  checkElementIsSafe(array, row, col, num) {
+    return !this.checkRow(array, row, num) &&
+      !this.checkColumn(array, col, num) &&
       !this.checkBox3x3(array, row - row % 3, col - col % 3, num);
   }
-
-  // getUnassignedLocation(array) {
-  //   for (let row = 0; row < array.length; row++) {
-  //     for (let col = 0; col < array.length; col++) {
-  //       if (array[row][col] === BLANK) {
-  //         return { row:}
-  //       }
-  //     }
-  //   }
-  // }
+  solveSudoku(array) {
+    let locationList = [0, 0];
+    if (!this.checkBlankElement(array, locationList)) {
+      return true;
+    }
+    let row = locationList[0];
+    let col = locationList[1];
+    // num loop try from 1 - 9
+    for (let num = 1; num <= 9; num++) {
+      // if location is promising and safe
+      if (this.checkElementIsSafe(array, row, col, num)) {
+        // assign value to 0 / BLANK
+        array[row][col] = num;
+        // return if success 
+        if (this.solveSudoku(array)) {
+          return true;
+        }
+        // failure, back again and try
+        array[row][col] = 0;
+      }
+    }
+    // backtracking here
+    return false;
+  }
 }
 
 // The file has newlines at the end of each line,
