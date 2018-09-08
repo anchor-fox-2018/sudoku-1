648a156653b38cc7637c6f3f0c688a0795a05882
Lundy Orlando <simplyeazy@gmail.com>
2018-09-08 17:45:54 +0700

    Release 0 Not done yet, buggy but the board checking logic is done.

--- sudoku.js
+++ sudoku.js
@@ -1,42 +1,88 @@
 "use strict";
-const DIM = 9;
-const BLANK = 0;
+const BLANK = '0';
 const SPACE = ' ';
 const LINE = '|';
 const NEW_ROW = "-------------------------------------";
-/*
-"
-105802000
-090076405
-200400819
-019007306
-762083090
-000061050
-007600030
-430020501
-600308900
-"
- */
 class Sudoku {
   constructor(board_string) {
   }
-  board(){
+  board() {
     let board_string = `105802000090076405200400819019007306762083090000061050007600030430020501600308900`
-    let X = [];
-
-    for (let i = 0, lastPosition = 0; i <9; i++) {
+    // Fill the 
+    let arrResult = [];
+    for (let i = 0, lastPosition = 0; i < 9; i++) {
       let arrCol = [];
       for (let j = 0; j < 9; j++) {
         arrCol.push(board_string[lastPosition]);
         lastPosition++;
       }
-      X.push(arrCol);
+      arrResult.push(arrCol);
     }
-    console.log(X);
+    console.log(arrResult);
+    // check horizontal
+    this.solve(arrResult);
   }
-  solve() { }
-  checkVertical() { }
-  checkHorizontal() { }
+
+  solve(array) {
+    for (let row = 0; row < array.length; row++) {
+      for (let col = 0; col < array.length; col++) {
+        if (array[row][col]===BLANK) {
+          console.log(`Cell ${row} ${col} is BLANK`)
+          // masih ngebug
+        }
+      }
+    }
+
+    for (let num = 1; num <= 9; num++) {
+
+    }
+  }
+
+  checkVertical(array, col, num) {
+    for (let row = 0; row < array.length; row++) {
+      if (array[row][col] === num) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkHorizontal(array, row, num) {
+    for (let col = 0; col < array.length; col++) {
+      if (array[row][col] === num) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  checkBox3x3(array, boxStartRow, boxStartCol, num) {
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        if (array[row + boxStartRow][col + boxStartCol] === num) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  isSafe(array, row, col, num) {
+    return
+    !this.checkHorizontal(array, row, num) &&
+      !this.checkVertical(array, row, num) &&
+      !this.checkBox3x3(array, row - row % 3, col - col % 3, num);
+  }
+
+  // getUnassignedLocation(array) {
+  //   for (let row = 0; row < array.length; row++) {
+  //     for (let col = 0; col < array.length; col++) {
+  //       if (array[row][col] === BLANK) {
+  //         return { row:}
+  //       }
+  //     }
+  //   }
+  // }
 }
 
 // The file has newlines at the end of each line,
